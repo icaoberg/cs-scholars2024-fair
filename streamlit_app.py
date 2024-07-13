6199bac85998e8d10b91e2097478cacd66198df8
icaoberg <icaoberg@andrew.cmu.edu>
2024-07-13 16:29:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,10 +122,10 @@ from wordcloud import WordCloud
 text = ' '.join(df['group_name'].str.replace(' ', '_').tolist())
 
 # Create the Word Cloud
-wordcloud = WordCloud(width=200, height=300, background_color='white',  collocations = False,).generate(text)
+wordcloud = WordCloud(width=200, height=200, background_color='white',  collocations = False,).generate(text)
 
 # Create a figure
-fig, ax = plt.subplots(figsize=(3, 4))  # Create a figure of size 10 inches by 5 inches
+fig, ax = plt.subplots(figsize=(2, 3))  # Create a figure of size 10 inches by 5 inches
 
 # Display the Word Cloud
 ax.imshow(wordcloud, interpolation='bilinear')  # Display the word cloud image
