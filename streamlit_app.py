c4b13a3c1103ee920a81e53bd90f717a5575c972
Amitch55 <152036304+Amitch55@users.noreply.github.com>
2024-07-15 19:30:41 +0000

    Added Read Me Abstract

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ st.image(logo_url)
 title = '# Self-assessment of HuBMAP published resources'
 st.write(title)
 
+Read_me_abstract = "The Human BioMolecular Atlas Program (HuBMAP) aims to create a comprehensive, open, and accurate map of the human body at the cellular level. This initiative addresses the critical need for high-resolution, multi-dimensional data that can enhance our understanding of human biology and disease. To see if HuBMAP data follows the FAIR (Findable, Accessible, Interoperable, Reusable) principles, we analyzed how the data is managed and shared. We looked at the standards used for metadata, how easy it is to access the data, how well the data works with other datasets, and how reusable the data is. Our review showed that HuBMAP data is fairly straightforward to find thanks to metadata and strong search tools. The data can be accessed through user-friendly platforms and APIs, making it easy for anyone to use. The data is also designed to work well with other datasets due to standardized formats. Plus, the data is highly reusable, supported by clear licensing and thorough documentation. By making data findable, accessible, interoperable, and reusable, HuBMAP helps scientific discovery and sets a high standard for other big biological data projects."
+print(Read_me_abstract)
+
 authors = 'Prince, A. Tinajero, A. Perez, L. Ku, J. Li, X. Ricano, J. Fisher, M. Edmond, J. Mitchell, A. McLeod, A. Wong, A. Cao-Berg, I.'
 st.write(authors)
 
